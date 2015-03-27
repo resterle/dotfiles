2300f645448b4355c971b8930cc27aef10d313f8
Raphael Esterle <raphael.esterle@bayard-consulting.com>
2015-03-27 23:39:35 +0100

    install.sh added

---
+++ install.sh
@@ -0,0 +1,6 @@
+mv ~/.vimrc ~/.vimrc.bak
+ln -s $(pwd)/.vimrc ~/.vimrc
+mv ~/.bashrc ~/.bashrc.bak
+ln -s $(pwd)/.bashrc ~/.bashrc
+ln -s $(pwd)/git-completion.bash ~/git-completion.bash
+ln -s $(pwd)/git-prompt.sh ~/git-prompt.sh
