28c0614a7986a850eb9f97b65db5b2b890fd850c
Raphael Esterle <hansahanz@gmail.com>
2016-02-16 22:55:24 +0100

    aliases

---
+++ aliases.sh
@@ -0,0 +1,10 @@
+# some more ls aliases
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+alias gg='git log --graph --oneline --decorate'
+alias gga='git log --graph --oneline --decorate --all'
+alias gst='git status'
+alias tma='tmux attach-session -t'
+alias tml='tmux list-sessions'
+
